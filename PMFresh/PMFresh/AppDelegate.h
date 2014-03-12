2b06b85f3274c34a92c4e9bc06c58c77e2190a89
Igor Milakovic <igor.milakovic@gmail.com>
2014-03-12 23:50:50 +0100

    Xcode project

---
+++ PMFresh/PMFresh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PMFresh
+//
+//  Created by Igor Milakovic on 12/03/14.
+//  Copyright (c) 2014 Pliable Matter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PMFresh/PMFreshTests/PMFreshTests.m
@@ -0,0 +1,34 @@
+//
+//  PMFreshTests.m
+//  PMFreshTests
+//
+//  Created by Igor Milakovic on 12/03/14.
+//  Copyright (c) 2014 Pliable Matter. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PMFreshTests : XCTestCase
+
+@end
+
+@implementation PMFreshTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
